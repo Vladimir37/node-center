c85a80936ba9ea6064752c22cd9e362e1b6d49e2
Vladimir37 <vladimir_zapas@mail.ru>
2016-04-04 23:40:43 +0000

    Code

--- Current/client/source/scripts/main.js
+++ Current/client/source/scripts/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
     $('.accord_head').click(function() {
         $(this).next('.accord_body').slideToggle();
-    })
+    });
+    $('.code').each(function(i, block) {
+        hljs.highlightBlock(block);
+    });
 });
